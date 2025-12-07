4631c3faf820723f120db83b322bafc5f9450191
susmitamali1895 <mali.susmita09@gmail.com>
2025-12-07 10:03:43 +0530

    Add files via upload

---
+++ Example2.py
@@ -0,0 +1,3 @@
+# multiply argument a with argument b and return result
+x = lambda a, b: a*b
+print(x(5,6))
---
+++ Example3.py
@@ -0,0 +1,3 @@
+#summerise argument a, b, c and return thr result
+x = lambda a,b,c: a +b+c
+print(x(5,10,8))
